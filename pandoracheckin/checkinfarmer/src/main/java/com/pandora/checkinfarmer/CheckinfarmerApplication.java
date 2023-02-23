d456975a7a45b1d8cf214e585bf579b691ee868c
GabrielMSosa <sosagabriel79@gmail.com>
2023-02-23 00:53:30 -0300

    se comitea cambios y se crea servicio pandora

---
+++ pandoracheckin/checkinfarmer/src/main/java/com/pandora/checkinfarmer/CheckinfarmerApplication.java
@@ -0,0 +1,13 @@
+package com.pandora.checkinfarmer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckinfarmerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckinfarmerApplication.class, args);
+	}
+
+}
