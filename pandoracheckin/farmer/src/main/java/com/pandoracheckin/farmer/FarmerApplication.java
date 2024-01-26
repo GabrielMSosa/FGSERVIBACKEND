6c1879f11326bce3b5d52279e1bdf132334c5843
Gabriel Matias Sosa <sosagabriel79@gmail.com>
2024-01-26 16:40:12 -0300

    feat: se adapta los servicios para win11 y docker

--- pandoracheckin/farmer/src/main/java/com/pandoracheckin/farmer/FarmerApplication.java
+++ pandoracheckin/farmer/src/main/java/com/pandoracheckin/farmer/FarmerApplication.java
@@ -3,8 +3,8 @@ package com.pandoracheckin.farmer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
@@ -12,7 +12,7 @@ import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableFeignClients
- @EnableDiscoveryClient
+@EnableEurekaClient
 public class FarmerApplication {
 
     @Bean
